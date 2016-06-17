60938583bd26de04e6175c9eb75560ecb2dbbf5f
defaultbob <mills.85@gmail.com>
2016-06-17 13:48:30 -0700

    remove creds

--- API01.py
+++ API01.py
@@ -1,36 +1,26 @@
 """VAULT API TESTING 01"""
 
+import config
 import datetime
 import os
 import VaultAPI
 
-#domain = "pm8.vaultdev.com"
-domain = "z-team-promomats.veevavault.com"
-#username = "david.mills@mdltest.com"
-username = "david.mills@z-team.com"
-#password = "2:<A!zyRuyQr"
-password = "ZZkWCp0YZA"
-#version = 15
-version = 14
+session = VaultAPI.get_session(config.domain, config.username, config.password, config.version)
 
-test_mode = True
-
-session = VaultAPI.get_session(domain, username, password, version)
-
-comps = VaultAPI.get_component_types(domain, session)
+comps = VaultAPI.get_component_types(config.domain, session)
 
 instance_name = datetime.datetime.now()
-if test_mode:
+if config.test_mode:
     instance_name = "TEST %s" % instance_name
 
 for component_json in comps:
     component_type = component_json["type"]
-    type_folder = "output/%s/%s/%s" % (domain, instance_name, component_type)
+    type_folder = "output/%s/%s/%s" % (config.domain, instance_name, component_type)
 
     os.makedirs(type_folder)
     for component_name in component_json["names"]:
         with open(type_folder + "/" + component_name + ".mdl", "w") as f:
-            f.write(VaultAPI.get_component(domain, session,
+            f.write(VaultAPI.get_component(config.domain, session,
                                            component_type + "." + component_name))
-        if test_mode:
+        if config.test_mode:
             quit()
