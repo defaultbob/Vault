408995e30835eb97c20e2a00d6efe81be99f08b8
defaultbob <mills.85@gmail.com>
2016-06-22 14:09:22 -0700

    fixes

--- APIClient01.py
+++ APIClient01.py
@@ -7,20 +7,15 @@ import ApiClient
 import printProgress
 import VaultService
 
-# Read config
-configParser = ConfigParser.RawConfigParser()
-configParser.readfp(open(r'config.txt'))
-
-domain = configParser.get('credentials', 'domain')
-username = configParser.get('credentials', 'username')
-password = configParser.get('credentials', 'password')
-version = configParser.getint('credentials', 'version')
-test_mode = configParser.getboolean('credentials', 'test_mode')
-
-client = ApiClient.ApiClient(domain, username, password, version)
+client = VaultService.get_client()
 
 comps = VaultService.get_component_types(client)
 instance_name = datetime.datetime.now()
+
+# Read config
+configParser = ConfigParser.RawConfigParser()
+configParser.readfp(open(r'config.txt'))
+test_mode = configParser.getboolean('credentials', 'test_mode')
 
 if test_mode:
     instance_name = "TEST %s" % instance_name
@@ -37,8 +32,8 @@ printProgress.printProgress(
 
 for component_json in comps:
     component_type = component_json["type"]
-    type_folder = "../output/%s/%s/%s" % (domain,
-                                       instance_name, component_type)
+    type_folder = "../output/%s/%s/%s" % (client.domain,
+                                          instance_name, component_type)
 
     os.makedirs(type_folder)
     for component_name in component_json["names"]:
@@ -46,7 +41,7 @@ for component_json in comps:
         with open(type_folder + "/" + component_name + ".mdl", "w") as f:
             mdl = VaultService.get_component(
                 client, name)
-            f.write(mdl)
+            f.write(mdl.encode('utf-8'))
         i += 1
         printProgress.printProgress(
             i, l, prefix='Progress:', suffix='Complete' + " - " + name, barLength=50)
--- VaultService.py
+++ VaultService.py
@@ -1,5 +1,19 @@
 
-import requests
+import ConfigParser
+import ApiClient
+
+def get_client():
+    # Read config
+    configParser = ConfigParser.RawConfigParser()
+    configParser.readfp(open(r'config.txt'))
+
+    domain = configParser.get('credentials', 'domain')
+    username = configParser.get('credentials', 'username')
+    password = configParser.get('credentials', 'password')
+    version = configParser.getint('credentials', 'version')
+
+    client = ApiClient.ApiClient(domain, username, password, version)
+    return client
 
 def get_component_types(client):
     """Get Component Types"""
